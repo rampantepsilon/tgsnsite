5f33a8257b7dab8532c455f332d8523f09fcc584
Tom 'RampantEpsilon' Ware <tomjware@gmail.com>
2020-07-17 13:15:57 -0400

    Modified settings for page

--- public/scripts/discord.js
+++ public/scripts/discord.js
@@ -1,6 +1,6 @@
 
 function getMessages(){
-  setInterval("api()",30000)
+  setInterval("api()",5000)
 }
 
 function init(){
@@ -10,25 +10,24 @@ function init(){
 
 function api(){
   //console.log("Begin Script");
-  var token = "";
+  var token = "MzgwNTM3MzkzNjMzMDM0MjUx.XuzacQ.Wb6mKXps84S_fFu-aqA6qwyVZ0Y";
   $.ajax({
     datatype: 'json',
-    url: 'https://discordapp.com/api/channels/487679359566741504/messages',
+    url: 'https://discord.com/api/channels/487679359566741504/messages',
     headers: {
       "Authorization": 'Bot ' + token,
     },
     success: function(messages)
     {
       document.getElementById('test').innerHTML = '';
-      var i, author, message;
+      var i, id, message;
       for (i=0; i < messages.length; i++){
-        author = messages[i].author.username + ' #' + messages[i].author.discriminator;
         message = messages[i].content;
         console.log(message.substring(0,4));
 
         if (message.substring(0,4) == 'img '){
           message = message.substring(4,1000);
-          document.getElementById('test').innerHTML += `<table width='100%'><tr><td align='center' style='font-size:30px'>` + author + `</td></tr><td align='center'><img src='` + message + `' style='max-width: 100%; height: auto;'></td></tr></table>`;
+          document.getElementById('test').innerHTML += `<div align='center'><img src='` + message + `' style='max-width: 100%; height: auto;'></div>`;
         }
       }
     },
