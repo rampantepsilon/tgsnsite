6e50048f534433eb21941eef99cec8dd33fd75a3
Tom Ware <tomjware@gmail.com>
2021-12-29 11:14:49 -0500

    Updated Show Resources

--- public/index.js
+++ public/index.js
@@ -5,10 +5,16 @@ document.addEventListener("DOMContentLoaded", event =>{
   const db = firebase.firestore();
 
   const tgsArticles = db.collection('tgs').doc('articles');
+  const tgsrArticles = db.collection('tgsr').doc('videos');
 
   tgsArticles.onSnapshot(doc => {
     const data = doc.data();
     document.querySelector('#tgsArticles').innerHTML = (`<iframe width='100%' height='100%' src="` + data.link + `" frameborder='0'>`)
+  })
+
+  tgsrArticles.onSnapshot(doc => {
+    const data = doc.data();
+    document.querySelector('#tgsrarticles').innerHTML = (`<h4 align='center' style='color:white;'>Watch Along @ <a href='` + data.source + `'>` + data.source + `</a><br><iframe width='100%' height='96%' src="` + data.notes + `" frameborder='0'>`)
   })
 
 });
